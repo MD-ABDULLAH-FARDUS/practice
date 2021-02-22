4ce3a03f8c5440abeb382b4a16ca21dd0177347a
SSB <abdullahferdus@gmail.com>
2021-02-22 21:55:17 +0600

    Updates

---
+++ static/js/main.4f4d8739.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppractice=this.webpackJsonppractice||[]).push([[0],{9:function(e,c,r){"use strict";r.r(c);r(1);var t=r(3),s=r.n(t),n=r(0);var a=function(){return Object(n.jsx)("div",{className:"card",children:Object(n.jsx)("h2",{className:"userName",children:"User Name"})})};var i=function(){return Object(n.jsxs)("div",{children:[Object(n.jsx)(a,{}),","]})};s.a.render(Object(n.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.4f4d8739.chunk.js.map
