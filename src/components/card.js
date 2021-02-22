7ff517b69e170873992f85da5cd320b0937a9dba
SSB <abdullahferdus@gmail.com>
2021-02-22 21:20:12 +0600

    first commit

---
+++ src/components/card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className = 'card'>
+            <h2 className='userName'>User Name</h2>
+        </div>
+    )
+}
+export default Card;
